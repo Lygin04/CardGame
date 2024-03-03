1564aa47e38708d842c2d6e0a75d58a9ac414390
Lygin04 <kosty.lygin.58@yandex.ru>
2024-03-03 18:15:28 +0300

    Add CardDraw script

---
+++ Assets/Code/CardDraw.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class CardDraw : MonoBehaviour
+{
+    private Game _game;
+    
+    [SerializeField] private GameSettings _gameSettings;
+
+    [SerializeField] private GameObject _cardPref;
+    [SerializeField] private List<GameObject> _hands;
+    
+    
+    public void Initialized()
+    {
+        _game = GameObject.Find("Game").GetComponent<Game>();
+        for (int i = 0; i < _gameSettings.CountPlayer; i++)
+        {
+            for (int j = 0; j < _game.players[i].cards.Count; j++)
+            {
+                GameObject newCard = Instantiate(_cardPref, _hands[i].transform);
+                newCard.GetComponent<Image>().sprite = _gameSettings.Deck[_game.players[i].cards[j]].Image;
+            }
+        }
+    }
+
+    private void Update()
+    {
+       
+    }
+}
